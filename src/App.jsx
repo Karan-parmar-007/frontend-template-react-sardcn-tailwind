8b07f7529605f4f6888747b3da94e0ad3dbbb092
Karan-parmar-007 <karanparmar00014@gmail.com>
2025-10-18 09:45:11 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'  // Now works with alias
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 p-4">
+      <Button>Hello, Shadcn!</Button>
+    </div>
+  )
+}
+
+export default App
